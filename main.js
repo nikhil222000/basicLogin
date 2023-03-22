188a3360c93f144ff0bf25001a0eb59cd1033581
nikhil222000 <70390692+nikhil222000@users.noreply.github.com>
2023-03-22 10:46:50 +0530

    Login Task

--- main.js
+++ main.js
@@ -5,19 +5,20 @@ document.addEventListener("DOMContentLoaded", () => {
   if(document.getElementById ("show")){
   document.getElementById ("show").addEventListener ("click", passwordHides, false);
   }
+  function passwordHides() {
+    if (pass_field.type === "password") {
+      pass_field.type = "text";
+      showBtn.textContent = "HIDE";
+      showBtn.style.color = "#3498db";
+    } else {
+      pass_field.type = "password";
+      showBtn.textContent = "SHOW";
+      showBtn.style.color = "#222";
+    }
+  };
 });
 //Function used for password show hide
-function passwordHides() {
-  if (pass_field.type === "password") {
-    pass_field.type = "text";
-    showBtn.textContent = "HIDE";
-    showBtn.style.color = "#3498db";
-  } else {
-    pass_field.type = "password";
-    showBtn.textContent = "SHOW";
-    showBtn.style.color = "#222";
-  }
-};
+
 
 
 
